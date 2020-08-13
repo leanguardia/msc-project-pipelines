71bdc83fd6c344eb36ddcf0c7ca1e2ff9f3a2bb9
Leandro Guardia <lean.guardia93@gmail.com>
2020-08-13 22:48:05 +0100

    ETL: Forest Fires

--- pipelines/etl_forest_fires.py
+++ pipelines/etl_forest_fires.py
@@ -3,38 +3,23 @@ import numpy as np
 
 from dummy_transformer import dummify
 
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///lake/warehouse.db')#, echo=True)
+
 def load_data(filepath):
     df = pd.read_csv(filepath)
     return df
 
-# categorical_cols = ['month', 'day']
-# numerical_cols = [col for col in df.columns.tolist() if not col in categorical_cols]
-
-# ### Forest Weather Index (FWI)
-# Is a Canadian system for rating fire danter. It includes six components (Cortez et al).
-# - Fine Fuel Moisture Code (FFMC): moisture content surface litter and influences ignition and fire spread
-# - Initial Spread Index (ISI): Fire velocity spread
-# - Duff Moisture Code (DMC): moisture content of shallow organic layers
-# - Drought Code (DC): moisture content of deep organic layer
-
-# ### Weather Indicators
-# - temp: Temperature (Celcius)
-# - RH: Relative humidity (%)
-# - wind (km/h)
-# - rain (mm/m2
-
-# Remove Rain Outliers
-# sorted_rain = df.rain.sort_values(ascending=False)
-# print(sorted_rain.head(10))
-# print(sorted_rain.tail(5))
-# df = df[df.rain < 6.0]
-
-# ### Target: Burnt Are
-# Remove Outliers
-# max_area = 70
-# df = df[df.area < max_area]
-
 if __name__ == "__main__":
+    
+    print("Extracting Data")
     df = load_data('lake/forest_fires/forestfires.csv')
+    
+    print("Transforming Data")
     df = dummify(df, 'month', prefix='month')
     df = dummify(df, 'day', prefix='day')
+
+    print("Loading Data")
+    df.to_sql("fires", engine, if_exists='replace')
+    print("ETL - Done")
