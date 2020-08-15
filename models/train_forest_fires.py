a8161a19a61295e2c162a4f4a99a3a9925899e47
Leandro Guardia <lean.guardia93@gmail.com>
2020-08-15 17:38:53 +0100

    Pipe: ArgParse for Forest Model Training

--- models/train_forest_fires.py
+++ models/train_forest_fires.py
@@ -1,8 +1,8 @@
+import argparse
+
 import pandas as pd
 import numpy as np
-
 from sqlalchemy import create_engine
-
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
@@ -10,14 +10,14 @@ from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
 
 from util.io import store_model
 
-def load_data(database_filepath):
-    engine = create_engine(f'sqlite:///{database_filepath}')
-    df = pd.read_sql_table("fires", engine)
+def load_data(database, table):
+    engine = create_engine(f'sqlite:///{database}')
+    df = pd.read_sql_table(table, engine)
     features  = ['X', 'Y', 'FFMC', 'DMC', 'DC', 'ISI', 'temp', 'RH', 'wind', 'rain',
                 'month_jan', 'month_feb', 'month_mar', 'month_apr', 'month_may', 'month_jun',
                 'month_jul', 'month_aug', 'month_sep', 'month_oct', 'month_nov', 'month_dec', 
                 'day_mon', 'day_tue', 'day_wed', 'day_thu', 'day_fri', 'day_sat', 'day_sun']
-    target = "area"
+    target = 'area'
     X = df[features]
     y = df[target]
     return X, y
@@ -28,17 +28,6 @@ def evaluate(y_test, y_pred):
     print('(MSE) Mean squared error: %.2f' % mean_squared_error(y_test, y_pred))
     print('(R2) Coefficient of determination: %.2f' % r2_score(y_test, y_pred))
     
-#     a = np.concatenate((y_test.values.reshape(-1,1), y_pred.reshape(-1,1)), axis=1)
-#     a = a[a[:,0].argsort()]
-
-#     fig, ax = plt.subplots(figsize=(15,5))
-#     x_ticks = range(y_test.shape[0])
-#     ax.scatter(x_ticks, a[:,0], label='Actual', c='turquoise')
-#     ax.scatter(x_ticks, a[:,1], label='Predictions', s=15, c='orange');
-#     ax.legend()
-# evaluate(y_test, y_pred)
-
-
 
 # def save_model(model, model_filepath):
 #     '''
@@ -51,28 +40,50 @@ def evaluate(y_test, y_pred):
 #     dump(model, model_filepath)
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Training of Forest Fires predictive model.')
+        
+    parser.add_argument('model', type=str,# dest='model_filepath',
+        help='Output model name (e.g. models/regression.pkl)')
 
-    X, y = load_data('lake/warehouse.db')
+    default_db = 'lake/warehouse.db'
+    parser.add_argument('-d', '--database',
+        type=str, dest='database', default=default_db,
+        help=f'SQLite database file to query data from (default: {default_db})')
+
+    default_table = 'fires'
+    parser.add_argument('-t', '--table', type=str, default=default_table,
+        help=f'Database table to query (default: {default_table})')
+
+    args = vars(parser.parse_args())
+ 
+    print('≫ Loading data')
+    database = args['database']
+    db_table = args['table']
+    X, y = load_data(database, db_table)
+
+    print('≫ Feature Engineering')
 
     # Remove Outliers
     # Polynomial Data
     # Transform to Log Scale
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=25)
-
+    print('≫ Training Model')
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25,
+                                                        random_state=25)
     model = LinearRegression()
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
 
+    print('≫ Evaluation Summary')
     print('(MAE) Mean absolute error: %.2f' % mean_absolute_error(y_test, y_pred))
     print('(MSE) Mean squared error: %.2f' % mean_squared_error(y_test, y_pred))
     print('(R2) Coefficient of determination: %.2f' % r2_score(y_test, y_pred)) 
 
-    print('Storing Regressor')
-    store_model(model, 'models/regressor.pkl')
-    
-
+    model_filepath = args['model']
+    print(f'≫ Storing Model "{model_filepath}"')
+    store_model(model, model_filepath)
 
 # Polynomial transformations
 # X_train_polyer = PolynomialFeatures(degree=2)
