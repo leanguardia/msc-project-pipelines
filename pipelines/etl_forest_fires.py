55144cbdb96b4820a269590539995d0866d51e8a
Leandro Guardia <lean.guardia93@gmail.com>
2020-08-15 17:05:51 +0100

    Pipe: ArgParse Fores Fires

--- pipelines/etl_forest_fires.py
+++ pipelines/etl_forest_fires.py
@@ -1,25 +1,41 @@
+import argparse
+
 import pandas as pd
 import numpy as np
-
-from dummy_transformer import dummify
-
 from sqlalchemy import create_engine
 
-engine = create_engine('sqlite:///lake/warehouse.db')#, echo=True)
+from dummy_transformer import dummify
 
 def load_data(filepath):
     df = pd.read_csv(filepath)
     return df
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Preparation of Forest Fires dataset.')
+
+    default_data = 'lake/forest_fires/forestfires.csv'
+    parser.add_argument('-s', '--source', type=str, default=default_data,
+        dest='data', help=f'filepath for data source (default: {default_data})')
     
+    default_db = 'lake/warehouse.db'
+    parser.add_argument('-d', '--database',
+        type=str, dest='database', default=default_db,
+        help=f'SQLite database file to store result (default: {default_db})')
+
+    args = vars(parser.parse_args())
+    print(args)
+
     print("Extracting Data")
-    df = load_data('lake/forest_fires/forestfires.csv')
+    data_source = args['data']
+    df = load_data(data_source)
     
     print("Transforming Data")
     df = dummify(df, 'month', prefix='month')
     df = dummify(df, 'day', prefix='day')
 
     print("Loading Data")
+    database = args['data']
+    engine = create_engine('sqlite:///lake/warehouse.db')
     df.to_sql("fires", engine, if_exists='replace')
     print("ETL - Done")
