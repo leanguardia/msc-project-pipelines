fc941e4e2e73d4686196f1e6ea5905eec7dc4ac1
Leandro Guardia <lean.guardia93@gmail.com>
2020-08-15 22:22:46 +0100

    App: Split Index and ForesFires

--- app/app.py
+++ app/app.py
@@ -1,38 +1,21 @@
 from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from sklearn.externals import joblib
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
+
+model = joblib.load("models/forestfires.pkl")
 
 
-class Fire(db.Model):
-    id          = db.Column(db.Integer, primary_key=True)
-    rain        = db.Column(db.Integer, default=0)
-    temperature = db.Column(db.Integer, default=0)
-    created_at  = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Fire %r>' % self.id
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
-    if request.method == 'POST':
-        rain = request.form['rain']
-        temperature = request.form['temperature']
-        new_fire = Fire(rain=rain, temperature=temperature)
-        try:
-            db.session.add(new_fire)
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was a Problem!'
-    else:
-        fires = Fire.query.order_by(Fire.created_at).all()
-        return render_template('index.html', fires=fires)
+        return render_template('index.html')
 
+
+@app.route('/forestfires')
+def forest_fires():
+    return render_template('forestfires.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
