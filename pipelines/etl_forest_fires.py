72c6d79ec0fb099b642d95ebb89b8b244d4332ea
Leandro Guardia <lean.guardia93@gmail.com>
2020-08-17 01:47:43 +0100

    Pipe: Lower Bound Inference Engine

--- pipelines/etl_forest_fires.py
+++ pipelines/etl_forest_fires.py
@@ -35,9 +35,17 @@ class ForestFirePredictor():
         self.model = model
 
     def predict(self, X, decimals=2):
-        """ Performs one Prediction."""
+        """ Performs one Prediction.
+        
+        Parameters:
+        - X: Input array compatible with model interface for inference.
+        - decimals (int): Number of decimals required
+
+        Returns: (int) Prediction result. 
+        """
 
         prediction = self.model.predict(X)[0]
+        if prediction < 0: prediction = 0
         return [round(prediction, decimals)]
 
 if __name__ == "__main__":
--- test/test_forest_fire_predictor.py
+++ test/test_forest_fire_predictor.py
@@ -7,6 +7,7 @@ from unittest.mock import Mock
 
 cols = ['X','Y','month','day','FFMC','DMC','DC','ISI','temp','RH','wind','rain']
 vals =    [1, 2, 'dec', 'tue', 20.0,  100.0, 400, 35, 33.2, 60, 5, 4]
+# TODO: Transform Raw Args to Features for prediction.
 
 model = Mock()
 model.predict.return_value = [23.4324]
@@ -14,14 +15,6 @@ model.predict.return_value = [23.4324]
 def number_of_decimals(num):
     num_str = str(num)
     return len(num_str) - num_str.index('.') - 1
-
-# @patch('pipelines.etl_forest_fires.ForestFirePredictor')
-# def test_returns_zero_as_lower_bound(predictor_mock):
-#     predictor = predictor_mock.return_value
-#     engine = ForestFirePredictor(model)
-#     engine.predict([vals])
-#     predictor.predict.return_value = [24.234234]
-#     predictor.predict.assert_called_once()
 
 def test_return_a_float():
     engine = ForestFirePredictor(model)
@@ -39,3 +32,9 @@ def test_return_specified_decimal_points():
     inference = engine.predict([vals], decimals=4)[0]
     assert number_of_decimals(inference) == 4, 'number of decimal points \
         should be 4'
+
+def test_returns_zero_as_lower_bound():
+    model.predict.return_value = [-23.4324]
+    engine = ForestFirePredictor(model)
+    inference = engine.predict([vals])[0]
+    assert inference == 0, 'Inference lower bound should be zero'
