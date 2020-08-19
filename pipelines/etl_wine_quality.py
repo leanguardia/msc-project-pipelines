1f67ad9827361843dac292410ecbc879491aa877
Leandro Guardia <lean.guardia93@gmail.com>
2020-08-19 23:28:05 +0100

    Pipe: Wine argparser happy paths

--- pipelines/etl_wine_quality.py
+++ pipelines/etl_wine_quality.py
@@ -1,3 +1,5 @@
+import argparse 
+
 import numpy as np
 
 class WineQualityProcessor():
@@ -6,3 +8,14 @@ class WineQualityProcessor():
                   total_sulfur_dioxide, density, pH, sulphates, alcohol):
         return np.array([7.0, 0.27, 0.36, 20.7, 0.045, 45, \
                           170.0, 1.0010, 3.00, 0.45, 8.8])
+
+def parse_args(args=[]):
+    parser = argparse.ArgumentParser()
+
+    default_data = 'lake/wine_quality/wine_quality_white.csv'
+    parser.add_argument('-s', '--source', default=default_data, dest='data')
+
+    default_db = 'lake/warehouse.db'
+    parser.add_argument('-d', '--database', default=default_db, dest='database')
+        
+    return vars(parser.parse_args(args))
---
+++ test/test_wine_quality_etl.py
@@ -0,0 +1,26 @@
+from pipelines.etl_wine_quality import parse_args
+
+class TestWineQualityETL:
+    def test_argparser_default_data_source(self):
+        args = parse_args()
+        assert args['data'] == 'lake/wine_quality/wine_quality_white.csv'
+
+    def test_argparser_valid_data_source(self):
+        args = parse_args(['-s', 'specific/path/to/data.csv'])
+        assert args['data'] == 'specific/path/to/data.csv'
+
+    def test_argparser_valid_data_source_long(self):
+        args = parse_args(['--source', 'specific/path/to/data.csv'])
+        assert args['data'] == 'specific/path/to/data.csv'
+
+    def test_argparser_default_output_database(self):
+        args = parse_args()
+        assert args['database'] == 'lake/warehouse.db'
+
+    def test_argparser_valid_database(self):
+        args = parse_args(['-d', 'specific/path/to/database.db'])
+        assert args['database'] == 'specific/path/to/database.db'
+
+    def test_argparser_valid_database_lomg(self):
+        args = parse_args(['--database', 'specific/path/to/database.db'])
+        assert args['database'] == 'specific/path/to/database.db'
