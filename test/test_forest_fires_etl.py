957f344459f19dbb7816ec55e0ee72aa0feb0f73
Leandro Guardia <lean.guardia93@gmail.com>
2020-09-01 18:14:36 +0100

    Refactor: Tests

--- test/test_forest_fires_etl.py
+++ test/test_forest_fires_etl.py
@@ -5,27 +5,27 @@ from pipelines.etl_forest_fires import parse_args
 class TestForestFiresETL(TestCase):
     def test_argparser_default_data_source(self):
         args = parse_args()
-        assert args['data'] == 'lake/forest_fires/forestfires.csv'
+        self.assertEqual(args['data'], 'lake/forest_fires/forestfires.csv')
 
     def test_argparser_valid_data_source(self):
         args = parse_args(['-i', 'specific/path/to/data.csv'])
-        assert args['data'] == 'specific/path/to/data.csv'
+        self.assertEqual(args['data'], 'specific/path/to/data.csv')
 
     def test_argparser_valid_data_source_long(self):
         args = parse_args(['--input', 'specific/path/to/data.csv'])
-        assert args['data'] == 'specific/path/to/data.csv'
+        self.assertEqual(args['data'], 'specific/path/to/data.csv')
 
     def test_argparser_default_output_database(self):
         args = parse_args()
-        assert args['database'] == 'lake/warehouse.db'
+        self.assertEqual(args['database'], 'lake/warehouse.db')
 
     def test_argparser_valid_database(self):
         args = parse_args(['-d', 'specific/path/to/database.db'])
-        assert args['database'] == 'specific/path/to/database.db'
+        self.assertEqual(args['database'], 'specific/path/to/database.db')
 
     def test_argparser_valid_database_lomg(self):
         args = parse_args(['--database', 'specific/path/to/database.db'])
-        assert args['database'] == 'specific/path/to/database.db'
+        self.assertEqual(args['database'], 'specific/path/to/database.db')
 
     def test_argparser_default_db_table(self):
         args = parse_args()
--- test/test_wine_quality_processor.py
+++ test/test_wine_quality_processor.py
@@ -1,4 +1,5 @@
-import unittest
+from unittest import TestCase
+
 import pytest
 import pandas as pd
 import numpy as np
@@ -9,37 +10,37 @@ cols = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
         'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
         'pH', 'sulphates', 'alcohol', 'quality']
 values = [7.0, 0.27, 0.36, 20.7, 0.045, 45, 170.0, 1.0010, 3.00, 0.45, 8.8, 6]
-values_np = np.array(values)
-values_np2 = values_np.reshape(1, len(values_np))
+np_values = np.array(values)
+np_values_2d = np_values.reshape(1, len(np_values))
 df = pd.DataFrame([values], columns=cols)
 
-class TestWineQualityProcessor(unittest.TestCase):
+class TestWineQualityProcessor(TestCase):
     def setUp(self):
         self.processor = WineQualityProcessor()
 
     def test_transform_list(self):
-        transformed = self.processor.transform([values_np])
-        assert np.array_equal(transformed.values, values_np2)
+        transformed = self.processor.transform([values])
+        assert np.array_equal(transformed.values, np_values_2d)
 
     def test_transform_narray_one_dim(self):
         assert np.array_equal(
-            self.processor.transform(values_np).values, values_np2)
+            self.processor.transform(np_values).values, np_values_2d)
 
     def test_transform_narray_two_dims(self):
         assert np.array_equal(
-            self.processor.transform(values_np2).values, values_np2)
+            self.processor.transform(np_values_2d).values, np_values_2d)
 
     def test_transform_df(self):
-        assert np.array_equal(self.processor.transform(df).values, values_np2)
+        assert np.array_equal(self.processor.transform(df).values, np_values_2d)
 
     def test_transform_requires_minimum_number_of_features(self):
         with pytest.raises(ValueError, match='must have 11 or 12 columns'):
-            self.processor.transform([7.0, 0.27, 0.36, 20.7, 0.045, 45])
+            self.processor.transform(values[:6])
 
     def test_transform_requires_maximum_number_of_features(self):
         with pytest.raises(ValueError, match='must have 11 or 12 columns.'):
-            self.processor.transform([7.0, 0.27, 0.36, 20.7, 0.045, 45] * 3)
+            self.processor.transform(values * 2)
 
     def test_transform_accepts_only_features_as_input(self):
-        X = values_np[:-1]
+        X = np_values[:-1]
         assert np.array_equal(self.processor.transform([X]).values, [X])
