abba500357890403befc86853623d1675f8a2041
Leandro Guardia <lean.guardia93@gmail.com>
2020-09-12 02:18:55 +0100

    Wine Quality: Test All possible inputs

--- test/test_wine_quality_processor.py
+++ test/test_wine_quality_processor.py
@@ -6,17 +6,19 @@ import numpy as np
 
 from pipelines.wine_quality_etl import WineQualityProcessor
 
-cols = ['fixed_acidity', 'volatile_acidity', 'citric_acid', 'residual_sugar',
-        'chlorides', 'free_sulfur_dioxide', 'total_sulfur_dioxide', 'density',
-        'pH', 'sulphates', 'alcohol', 'quality']
+target_name = 'quality'
+input_names = ['fixed_acidity', 'volatile_acidity', 'citric_acid', 'residual_sugar',
+               'chlorides', 'free_sulfur_dioxide', 'total_sulfur_dioxide', 'density',
+               'pH', 'sulphates', 'alcohol']
 inputs = [7.0, 0.27, 0.36, 20.7, 0.045, 45.0, 170.0, 1.0010, 3.00, 0.45, 8.8, 6]
 
-np_inputs = np.array(inputs)
+np_inputs    = np.array(inputs)
 np_inputs_2d = np_inputs.reshape(1, len(np_inputs))
+df_inputs    = pd.DataFrame([inputs], columns=input_names + [target_name])
 
+feature_names = input_names + [target_name]
 feature_vals = inputs
-df_features = pd.DataFrame([feature_vals], columns=cols)
-
+df_features = pd.DataFrame([feature_vals], columns=feature_names)
 
 class TestWineQualityProcessor(TestCase):
     def setUp(self):
@@ -26,24 +28,25 @@ class TestWineQualityProcessor(TestCase):
         transformed = self.processor.transform(inputs)
         assert transformed.equals(df_features)
 
-    # def test_transform_wrapped_list(self):
-    #     transformed = self.processor.transform([inputs])
-    #     assert np.array_equal(transformed.inputs, np_inputs_2d)
+    def test_transform_wrapped_list(self):
+        transformed = self.processor.transform([inputs])
+        assert transformed.equals(df_features)
 
-    # def test_transform_narray_one_dim(self):
-    #     assert np.array_equal(
-    #         self.processor.transform(np_inputs).inputs, np_inputs_2d)
+    def test_transform_narray_one_dim(self):
+        transformed = self.processor.transform(np_inputs)
+        assert transformed.equals(df_features)
 
-    # def test_transform_wrapped_narray_one_dim(self):
-    #     assert np.array_equal(
-    #         self.processor.transform([np_inputs]).inputs, np_inputs_2d)
+    def test_transform_wrapped_narray_one_dim(self):
+        transformed = self.processor.transform([np_inputs])
+        assert transformed.equals(df_features)
+    
+    def test_transform_narray_two_dims(self):
+        transformed = self.processor.transform(np_inputs_2d)
+        assert transformed.equals(df_features)
 
-    # def test_transform_narray_two_dims(self):
-    #     assert np.array_equal(
-    #         self.processor.transform(np_inputs_2d).inputs, np_inputs_2d)
-
-    # def test_transform_df(self):
-    #     assert np.array_equal(self.processor.transform(df).inputs, np_inputs_2d)
+    def test_transform_df(self):
+        transformed = self.processor.transform(df_inputs)
+        assert transformed.equals(df_features)
 
     # def test_transform_requires_minimum_number_of_features(self):
     #     with pytest.raises(ValueError, match='must have 11 or 12 columns'):
@@ -55,4 +58,4 @@ class TestWineQualityProcessor(TestCase):
 
     # def test_transform_accepts_only_features_as_input(self):
     #     X = np_inputs[:-1]
-    #     assert np.array_equal(self.processor.transform([X]).inputs, [X])
+    #     assert np.array_equal(self.processor.transform([X]).val, [X])
