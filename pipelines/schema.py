45691ad00f916c66099feec6087a1f5cacf65f85
Leandro Guardia <lean.guardia93@gmail.com>
2020-09-26 14:42:51 +0100

    Schema: Num of columns and inputs

--- pipelines/schema.py
+++ pipelines/schema.py
@@ -1,3 +1,6 @@
+import pandas as pd
+import numpy as np
+
 class Schema:
     
     def __init__(self, schema_dict):
@@ -5,9 +8,15 @@ class Schema:
 
     def columns(self):
         return list(map(lambda column: column['name'], self.schema_dict))
+
+    def n_columns(self):
+        return len(self.columns())
     
     def inputs(self):
         return self.columns()[:-1]
+
+    def n_inputs(self):
+        return self.n_columns()-1
 
     def target(self):
         for feature_dict in self.schema_dict:
@@ -16,3 +25,23 @@ class Schema:
         raise ValueError('Target variable not found.')
     
             
+# def build_df(data, schema):
+#     if not type(data) == pd.DataFrame:
+#             data = np.array(data, ndmin=2)
+        
+#     _rows, cols = data.shape
+#     if cols < self.num_of_features or cols > self.num_of_columns:
+#         raise ValueError(f"incorrect number of columns")
+
+#     if cols == self.num_of_columns:
+#         columns = self.COLUMNS
+#         dtypes = self.DTYPES
+#     else:
+#         columns = self.COLUMNS[:-1]
+#         dtypes = self.DTYPES[:-1]
+
+#     df = pd.DataFrame(data, columns=columns)
+#     for col, dtype in zip(columns, dtypes):
+#         df[col]= df[col].astype(dtype)
+
+#     return df
--- test/test_schema.py
+++ test/test_schema.py
@@ -1,7 +1,7 @@
 import pytest
 import unittest
 
-from pipelines.schema import Schema
+from pipelines.schema import Schema#, build_df
 
 features_list = [
     dict(
@@ -41,8 +41,14 @@ class TestSchema(unittest.TestCase):
     def test_schema_columns(self):
         assert self.schema.columns() == names
 
+    def test_num_of_columns(self):
+        assert self.schema.n_columns() == 5
+
     def test_schema_inputs(self):
         assert self.schema.inputs() == names[:-1]
+
+    def test_num_of_inputs(self):
+        assert self.schema.n_inputs() == 4
 
     def test_schema_target(self):
         assert self.schema.target() == 'target'
@@ -51,4 +57,49 @@ class TestSchema(unittest.TestCase):
         failed_schema = Schema(features_list[:-1])
         with pytest.raises(ValueError, match='Target variable not found.'):
              assert failed_schema.target()
+
+
+# class TestBuildDf(unittest.TestCase):
+#     def setUp(self):
+#         self.schema = Schema(features_list)
+
+#     def test_build_df(self):
+#         df  = build_df([inputs], self.schema)
+#         assert df.columns == names
+
+    # def test_transform_list(self):
+    #     transformed = self.processor.transform(inputs)
+    #     assert transformed.equals(df_features)
+
+    # def test_transform_wrapped_list(self):
+    #     transformed = self.processor.transform([inputs])
+    #     assert transformed.equals(df_features)
+
+    # def test_transform_narray_one_dim(self):
+    #     transformed = self.processor.transform(np_inputs)
+    #     assert transformed.equals(df_features)
+    
+    # def test_transform_wrapped_narray_one_dim(self):
+    #     transformed = self.processor.transform([np_inputs])
+    #     assert transformed.equals(df_features)
+
+    # def test_transform_narray_two_dims(self):
+    #     transformed = self.processor.transform(np_inputs_2d)
+    #     assert transformed.equals(df_features)
+
+    # def test_transform_df(self):
+    #     transformed = self.processor.transform(df_inputs)
+    #     assert transformed.equals(df_features)
+
+    # def test_transform_requires_minimum_number_of_features(self):
+    #     with pytest.raises(ValueError, match="incorrect number of columns"):
+    #         self.processor.transform(inputs[:6])
+
+    # def test_transform_requires_maximum_number_of_features(self):
+    #     with pytest.raises(ValueError, match='incorrect number of columns'):
+    #         self.processor.transform(inputs * 2)
+
+    # def test_transform_features_only(self):
+    #     transformed = self.processor.transform([X])
+    #     assert transformed.equals(df_features[input_names + new_feature_names[1:]])
 
