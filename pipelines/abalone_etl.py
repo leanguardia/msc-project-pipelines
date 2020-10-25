741f2fb0e4ce2c9fb535e2937c8d2a0b62ee33ab
Leandro Guardia <lean.guardia93@gmail.com>
2020-10-25 20:48:26 +0000

    Rename ETL File

--- pipelines/abalone_etl.py
+++ pipelines/abalone_etl.py
@@ -4,16 +4,6 @@ import argparse
 import pandas as pd
 from sqlalchemy import create_engine
 
-# Sex / nominal / -- / M, F, and I (infant)
-# Length / continuous / mm / Longest shell measurement
-# Diameter / continuous / mm / perpendicular to length
-# Height / continuous / mm / with meat in shell
-# Whole weight / continuous / grams / whole abalone
-# Shucked weight / continuous / grams / weight of meat
-# Viscera weight / continuous / grams / gut weight (after bleeding)
-# Shell weight / continuous / grams / after being dried
-# Rings / integer / -- / +1.5 gives the age in years 
-
 def parse_args(args=[]):
     parser = argparse.ArgumentParser(
         description='ETL Preparation Pipeline for Abalone data.')
