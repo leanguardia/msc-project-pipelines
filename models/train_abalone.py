97adbca0ef6e5a3c55b369bddaf1b7e2778cff6e
Leandro Guardia <lean.guardia93@gmail.com>
2020-10-26 14:59:52 +0000

    Train Abalone Regression

--- models/train_abalone.py
+++ models/train_abalone.py
@@ -2,21 +2,15 @@ import sys
 import argparse
 
 import numpy as np
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split, GridSearchCV
 
+from sklearn.linear_model import LinearRegression, Ridge
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+
+# from pipelines.transformation import remove_outliers_iqr, remove_outliers_zscore
 from models.io import load_table, store_model, is_valid_model_filepath
-from models.evaluators import evaluate_classification
-
-def load_data(database, table):
-    df = load_table(database, table)
-    # features = ['sex', 'length', 'diameter', 'height', 'whole_weight',
-    features = ['length', 'diameter', 'height', 'whole_weight',
-                'shucked_weight', 'viscera_weight', 'shell_weight']
-    X = df[features]
-    y = df['rings']
-    return X, y
-
+from models.evaluators import evaluate_regression
 
 def parse_args(args):
     if args == None: raise TypeError('An arguments list is required')
@@ -46,24 +40,53 @@ if __name__ == "__main__":
     args = parse_args(sys.argv[1:])
  
     print('≫ Loading data')
-    X, y = load_data(args['database'], args['table'])
+    df = load_table(args['database'], args['table'])
 
     print('≫ Feature Engineering')
-
-    # Handle Categorical Data
     # Remove Outliers
     # Polynomial Data
 
-    print('≫ Training Model')
+    features = ['length', 'diameter', 'height', 'whole_weight',
+                'shucked_weight', 'viscera_weight', 'shell_weight', 'M', 'F']
+    X = df[features]
+    y = df['rings']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25,
-                                                        random_state=25)
-    logreg = LogisticRegression()
-    logreg.fit(X_train, y_train)
+                                                        random_state=0)
+    print('≫ Training Models')
+    models = {}
 
-    y_pred = logreg.predict(X_test)
 
-    evaluate_classification(y_test, y_pred, np.arange(23))
+    print('Linear Regression')
+    linreg = LinearRegression()
+    parameters = {
+        'fit_intercept': [True, False],
+        'normalize':     [True, False],
+    }
+    model = GridSearchCV(linreg, param_grid=parameters)
+    model.fit(X_train, y_train) 
+    print('> Best parameters:', model.best_params_)
+    y_pred = model.predict(X_test)
 
+    r2 = evaluate_regression(y_test, y_pred)
+    models[r2] = model.best_estimator_
+
+    
+    print('Random Forest')
+    forest = RandomForestRegressor()
+    parameters = {
+        'n_estimators': [75, 100, 300],
+        'max_depth':    [2, 5, None],
+    }
+    model = GridSearchCV(forest, param_grid=parameters)
+    model.fit(X_train, y_train)
+    print('> Best parameters:', model.best_params_)
+    y_pred = model.predict(X_test)
+
+    r2 = evaluate_regression(y_test, y_pred)
+    models[r2] = model.best_estimator_
+
+
+    best = models[max(models)]
     model_filepath = args['model']
-    print(f'≫ Storing Model "{model_filepath}"')
-    store_model(logreg, model_filepath)
+    print(f'≫ Storing Model {type(best)} in "{model_filepath}"')
+    store_model(best, model_filepath)
