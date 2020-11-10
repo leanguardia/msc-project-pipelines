5c79b42f9352550cb658c6ca18ea1cfa2e3a3db2
Leandro Guardia <lean.guardia93@gmail.com>
2020-11-10 00:57:28 +0000

    Wine Quality Schema Update

--- pipelines/wine_quality_schema.py
+++ pipelines/wine_quality_schema.py
@@ -1,54 +1,19 @@
-features = [
-    dict(
-        name='fixed_acidity',
-        type=float,
-    ),
-    dict(
-        name='volatile_acidity',
-        type=float
-    ), 
-    dict(
-        name='citric_acid',
-        type=float
-    ), 
-    dict(
-        name='residual_sugar',
-        type=float
-    ), 
-    dict(
-        name='chlorides',
-        type=float
-    ), 
-    dict(
-        name='free_sulfur_dioxide',
-        type=float
-    ), 
-    dict(
-        name='total_sulfur_dioxide',
-        type=float
-    ), 
-    dict(
-        name='density',
-        type=float
-    ), 
-    dict(
-        name='pH',
-        type=float
-    ), 
-    dict(
-        name='sulphates',
-        type=float
-    ), 
-    dict(
-        name='alcohol',
-        type=float
-    ),
-    dict(
-        name='type',
-        type=str
-    ), 
-    dict(
-        name='quality',
-        type=int
-    ), 
+from pipelines.schema import Schema
+
+wine_features_meta = [ 
+    dict(name='fixed_acidity', dtype=float, type='input'),
+    dict(name='volatile_acidity', dtype=float, type='input'), 
+    dict(name='citric_acid', dtype=float, type='input'), 
+    dict(name='residual_sugar', dtype=float, type='input'), 
+    dict(name='chlorides', dtype=float, type='input'), 
+    dict(name='free_sulfur_dioxide', dtype=float, type='input'), 
+    dict(name='total_sulfur_dioxide', dtype=float, type='input'), 
+    dict(name='density', dtype=float, type='input'), 
+    dict(name='pH', dtype=float, type='input'), 
+    dict(name='sulphates', dtype=float, type='input'), 
+    dict(name='alcohol', dtype=float, type='input'),
+    dict(name='type', dtype=str, type='input'), 
+    dict(name='quality', dtype=int, type='target'),
 ]
+
+wines_schema = Schema(wine_features_meta)
