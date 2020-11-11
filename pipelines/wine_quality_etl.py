a9a547c06a31f2a80dd2fe1e4821c5166090b75d
Leandro Guardia <lean.guardia93@gmail.com>
2020-11-11 19:54:47 +0000

    Adult ETL

--- pipelines/wine_quality_etl.py
+++ pipelines/wine_quality_etl.py
@@ -20,6 +20,16 @@ def parse_args(args=[]):
     parser.add_argument('-d', '--database', default=default_db, dest='database',
         help=f'SQLite database file to store result (default: {default_db})')
 
+    default_db_table = 'wines'
+    parser.add_argument('-t', '--table', default=default_db_table,
+        dest='table_name',
+        help=f'SQLite database table name (default: {default_db_table})')
+
+    default_table_overwrite = False
+    parser.add_argument('-o', '--overwrite', default=default_table_overwrite,
+        dest='table_overwrite', type=bool,
+        help=f'Overwrite database table (default: {default_table_overwrite})')
+
     return vars(parser.parse_args(args))
 
 
@@ -36,7 +46,8 @@ if __name__ == "__main__":
 
     print("≫ Loading Data")
     database = args['database']
+    db_table = args['table_name']
     engine = create_engine(f'sqlite:///{database}')
-    df.to_sql('wines', engine, if_exists='replace', index=False)
-    print("≫ ETL - Done")
+    if_table_exists = 'replace' if args['table_overwrite'] else 'fail'
+    df.to_sql(db_table, engine, if_exists=if_table_exists, index=False)
 
